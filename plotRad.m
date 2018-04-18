d683ec57f5e374e7f7c7f5666a6d7eb2a4ba32c4
Robert Torzilli <torzillirobert@gmail.com>
2018-04-18 11:31:08 -0400

    broken conversion?, need to fix wavelengths

--- plotRad.m
+++ plotRad.m
@@ -1,8 +1,31 @@
-importdata("test.mat");
-plot(inputs.molecularWavelengths,outputs.pathRadiance)
+
+% Import data from .mat file
+importdata("fig6_6.mat");
+pathSize = size(outputs.pathRadiance);
+waveLength = linspace(6e-6,25e-6, pathSize(2));
+
+% Plot data, pre conversion
+figure
+plot(waveLength,outputs.pathRadiance)
 title('Wavelength Radiance vs MolecularWavelength')
 xlabel('Wavelength')
 ylabel('Wavelength Radiance')
 hold on
-plot(inputs.molecularWavelengths,outputs.blackbody)
+plot(waveLength,outputs.blackbody)
 
+% Convert data
+% Wavenumber is the inverse of wavelength eq 3.2
+waveNumber = waveLength.^-1;
+
+% Wavenumber is waveLength^2/10 * Wavelength Radiance
+% Wavenumber is 1/(10*waveNum^2) * Wavelength Radiance
+wavNumberRadiance = (outputs.pathRadiance.*(waveLength.^2))./10;
+
+% Plot data, post conversion
+figure
+plot(waveNumber,wavNumberRadiance)
+title('Wavenumber Radiance vs WaveNumber')
+xlabel('Wavenumber')
+ylabel('Wavenumber Radiance')
+hold on
+plot(waveNumber,outputs.blackbody)
