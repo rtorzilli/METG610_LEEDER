5fcef9bd54f4b98921666735f0994ce35ca20b29
Robert Torzilli <torzillirobert@gmail.com>
2018-04-25 09:05:08 -0400

    modtran is a pain

--- Figs8_1_to_8_3_Modtran.m
+++ Figs8_1_to_8_3_Modtran.m
@@ -4,15 +4,15 @@ made in prior code and figures
 %}
 % Import data from text file 
 path = 'C:\Users\Robert\Documents\School\METG610\METG610_LEEDER\MODTRAND_OUT';
-fig81Barrow = xlsread(path+"\PettyFIGS.xls",'fig81B');
-fig81Nauru = xlsread(path+"\PettyFIGS.xls",'fig81N');
-fig82A = xlsread(path+"\PettyFIGS.xls",'fig82A');
-fig82B = xlsread(path+"\PettyFIGS.xls",'fig82B');
-fig83A = xlsread(path+"\PettyFIGS.xls",'fig83A');
-fig83B = xlsread(path+"\PettyFIGS.xls",'fig83B');
-fig83C_Storm = xlsread(path+"\PettyFIGS.xls",'fig83C_Storm');
-fig83C_NoStorm = xlsread(path+"\PettyFIGS.xls",'fig83C_NoStorm');
-fig83D = xlsread(path+"\PettyFIGS.xls",'fig83D');
+fig81Barrow = xlsread(path+"\PettyFIGS.xlsx",'fig81B');
+fig81Nauru = xlsread(path+"\PettyFIGS.xlsx",'fig81N');
+fig82A = xlsread(path+"\PettyFIGS.xlsx",'fig82A');
+fig82B = xlsread(path+"\PettyFIGS.xlsx",'fig82B');
+fig83A = xlsread(path+"\PettyFIGS.xlsx",'fig83A');
+fig83B = xlsread(path+"\PettyFIGS.xlsx",'fig83B');
+fig83C_Storm = xlsread(path+"\PettyFIGS.xlsx",'fig83C_Storm');
+fig83C_NoStorm = xlsread(path+"\PettyFIGS.xlsx",'fig83C_NoStorm');
+%fig83D = xlsread(path+"\PettyFIGS.xls",'fig83D');
 % Note that conversion constant 1e7 comes from W to mW (1e3) 
 % and um to cm (1e4)
 
@@ -37,7 +37,7 @@ plot(fig82B(:,1),fig82B(:,8).*1e7);
 xlabel('Wavenumber [cm-1]')
 ylabel('Wavenumber Radiance [mW/m2*sr*cm-1]')
 legend("ModTran Grnd Looking Up")
-
+%{
 figure
 plot(fig83A(:,1),fig83A(:,8).*1e7);
 xlabel('Wavenumber [cm-1]')
@@ -64,3 +64,5 @@ plot(fig83D(:,1),fig83D(:,8).*1e7);
 xlabel('Wavenumber [cm-1]')
 ylabel('Wavenumber Radiance [mW/m2*sr*cm-1]')
 legend("ModTran Southern Iraq")
+
+%}
